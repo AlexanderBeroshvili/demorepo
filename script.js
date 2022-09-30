b3b6eed6ce576611828288bf57a867bac67d2911
unknown <AlexanderBeroshvili>
2022-09-30 19:02:38 +0400

    add javascript file

---
+++ script.js
@@ -0,0 +1,7 @@
+const firsthero="Alex";
+const secondhero="Superman";
+const thirdhero="Spiderman";
+const intro="World is dangerous place. we need many heroes. I can be one of them. my name is ";
+console.log(intro + firsthero);
+console.log(intro + secondhero);
+console.log(intro + thirdhero);
